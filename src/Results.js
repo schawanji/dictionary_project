055c5bab74aedf4efeabb128ddd47e7cd09d39bc
schawanji <35290804+schawanji@users.noreply.github.com>
2021-08-01 17:31:22 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,13 @@
 export default function Results(props) {
   if (props.results) {
-    console.log(props.results);
+    console.log(props);
     return (
       <div>
-        {" "}
         <div>{props.results.word} </div>
-        <div> {props.results.word}</div>
+        {props.results.meanings.map(function (definition, index) {
+          console.log(definition.definitions[0].definition);
+          return <div key={index}>{definition.definitions[0].definition}</div>;
+        })}
       </div>
     );
   } else {
